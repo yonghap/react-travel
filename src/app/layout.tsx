6caf1919d2c9ba68a69bfd2fb6c1a2a7b3e5adcb
Park Yong Ha <yonghapk@gmail.com>
2025-09-28 20:36:58 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import '@/styles/globals.css';
 import { ReactNode } from 'react';
 import QueryProvider from '@/components/QueryProvider'; // 클라이언트 컴포넌트
+import Header from '@/components/Header'
 import { Suspense } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -14,6 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta name="description" content="App Router 구조에서 메타데이터 설정 예제" />
       </head>
       <body className="bg-gray-100 p-5 sm:p-10">
+        <Header></Header>
         <div className="overflow-hidden max-w-[600px] m-auto bg-white font-pretendard rounded-2xl shadow-lg">
           <QueryProvider>
             {children}
