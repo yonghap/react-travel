1a29245627f3f3841dc93538e8a4ae10f7390a52
Park Yong Ha <yonghapk@gmail.com>
2026-02-22 22:17:30 +0900

    add

--- src/components/stats/CategoryChart.tsx
+++ src/components/stats/CategoryChart.tsx
@@ -1,20 +1,25 @@
 "use client";
 
-import { PieChart, Pie, Tooltip, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Tooltip, Cell, ResponsiveContainer } from "recharts";
 
 const data = [
   { name: "Cafe", value: 10 },
   { name: "Viewpoint", value: 6 },
   { name: "Temple", value: 4 },
 ];
+const colors = ["#6366f1", "#f59e0b", "#10b981", "#ef4444", "#ec4899"];
 
 export default function CategoryChart() {
   return (
     <div className="bg-white rounded-2xl p-5 border border-neutral-200">
-      <h3 className="font-medium mb-4">Place Categories</h3>
+      <h3 className=" mb-4 font-bold">방문 장소</h3>
       <ResponsiveContainer width="100%" height={220}>
         <PieChart>
-          <Pie data={data} dataKey="value" nameKey="name" />
+          <Pie data={data} dataKey="value" nameKey="name">
+            {data.map((_, index) => (
+              <Cell key={index} fill={colors[index]} />
+            ))}
+          </Pie>
           <Tooltip />
         </PieChart>
       </ResponsiveContainer>
--- src/components/stats/CountryChart.tsx
+++ src/components/stats/CountryChart.tsx
@@ -1,22 +1,38 @@
 "use client";
 
-import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  Tooltip,
+  Cell,
+  ResponsiveContainer,
+  CartesianGrid,
+} from "recharts";
 
 const data = [
-  { name: "Japan", value: 12 },
-  { name: "USA", value: 4 },
-  { name: "Taiwan", value: 3 },
+  { name: "일본", value: 12 },
+  { name: "미국", value: 4 },
+  { name: "태국", value: 3 },
+  { name: "대만", value: 3 },
+  { name: "대한민국", value: 3 },
 ];
+
+const colors = ["#6366f1", "#f59e0b", "#10b981", "#ef4444", "#ec4899"];
 
 export default function CountryChart() {
   return (
     <div className="bg-white rounded-2xl p-5 border border-neutral-200">
-      <h3 className="font-medium mb-4">Visited Countries</h3>
+      <h3 className=" mb-4 font-bold">방문 국가</h3>
       <ResponsiveContainer width="100%" height={220}>
         <BarChart data={data}>
           <XAxis dataKey="name" />
           <Tooltip />
-          <Bar dataKey="value" />
+          <Bar dataKey="value">
+            {data.map((_, index) => (
+              <Cell key={index} fill={colors[index]} />
+            ))}
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </div>
