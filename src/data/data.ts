eeeb9bf6726ed25ce5f4ced23fbc2e3bc7b42c13
Park Yong Ha <yonghapk@gmail.com>
2026-04-23 20:41:44 +0900

    add

--- src/data/data.ts
+++ src/data/data.ts
@@ -37,6 +37,7 @@ export const place = [
     id: 10,
     name: '싱가포르',
     year: '2024',
+    image: 'https://lh3.googleusercontent.com/d/1wquIjC51SFsMiVV3ZuyioRVXOS9eFVSZ',
   },
   {
     id: 9,
